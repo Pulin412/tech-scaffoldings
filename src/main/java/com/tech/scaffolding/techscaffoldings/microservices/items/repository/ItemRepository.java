75215deaac357a6b2ba76bf7f585fa2736e89221
Pulin <pulin4122001@gmail.com>
2024-02-03 22:22:52 +0100

    added k6-load-test as scaffolding project

--- src/main/java/com/tech/scaffolding/techscaffoldings/microservices/items/repository/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tech.scaffolding.techscaffoldings.microservices.items.repository;
-
-import com.tech.scaffolding.techscaffoldings.microservices.items.entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-}
