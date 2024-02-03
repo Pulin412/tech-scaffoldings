1260fbacaefc4be00bb0c23a9e415170b036c36b
Pulin <pulin4122001@gmail.com>
2024-02-03 22:55:09 +0100

    initial commit for async-apis

---
+++ async-apis/src/main/java/com/tech/scaffolding/asyncapis/AsyncApisApplication.java
@@ -0,0 +1,13 @@
+package com.tech.scaffolding.asyncapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncApisApplication.class, args);
+	}
+
+}
---
+++ async-apis/src/test/java/com/tech/scaffolding/asyncapis/AsyncApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.scaffolding.asyncapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
